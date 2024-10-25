971e719b6430ba58b12e7ea26f9276bcb9521a53
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-10-25 12:56:06 +0530

    ddded from validation to registration

--- src/pages/registrationPage/Registration.jsx
+++ src/pages/registrationPage/Registration.jsx
@@ -5,21 +5,47 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 export default function Registration() {
- const [firstName,setFirstName]=useState("");
- const [lastName,setLastName]=useState("");
- const [wtNumber,setWtNumber]=useState("");
- const [mobileNo,setMobileNo]=useState("");
- const [email,setEmail]=useState("");
- const [password,setPassword]=useState("");
- const [error, setError] = useState(false);
+ const [firstName,setFirstName]=useState('');
+ const [lastName,setLastName]=useState('');
+ const [wtNumber,setWtNumber]=useState('');
+ const [mobileNo,setMobileNo]=useState('');
+ const [email,setEmail]=useState('');
+ const [password,setPassword]=useState('');
+ const [errorWhNumber, setErrorWhNumber] = useState(false);
+ const [errorFirstName,setErrorFirstName]=useState(false);
+ const [errorLastName,setErrorLastName]=useState(false);
+ const [errorMobileNo,setErrorMobileNo]=useState(false);
+ const [errorEmail,setErrorEmail]=useState(false);
+ const [errorPassword,setErrorPassword]=useState(false);
 
  function handleSubmit(){
 
-    if(wtNumber==""){
-        setError(true);
-    }else{
-        setError(false);
+    if((wtNumber=="")){
+        setErrorWhNumber(true);
+       return;
     }
+    if(firstName==""){
+      setErrorFirstName(true);
+      return;
+    }
+    if(lastName==""){
+      setErrorLastName(true);
+      return;
+    }
+    if(mobileNo==""){
+      setErrorMobileNo(true);
+      return;
+    }
+    if(email==""){
+      setErrorEmail(true);
+      return;
+    }
+    if(password==""){
+      setErrorPassword(true);
+      return;
+    }
+
+
     const data={
         firstName:firstName,
         lastName:lastName,
@@ -47,8 +73,10 @@ export default function Registration() {
           })
      })
      .catch((e)=>{
+      console.log(e.response.data);
+      
         Swal.fire(
-            `Registration fail ! ${e.response.data.error}`
+            `Registration fail !`
           );
      })
  }
@@ -60,22 +88,22 @@ export default function Registration() {
         <h1 className="text-[40px] drop-shadow-lg mb-3 text-blue-50 font-bold">Register with us !</h1>
         <div>
           <div className="mb-3">
-          <TextField value={firstName} onChange={(e)=>{setFirstName(e.target.value)}} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="First name" variant="filled" />
+          <TextField value={firstName} onChange={(e)=>{setFirstName(e.target.value); firstName!=""?setErrorFirstName(false):setErrorFirstName(true)} } sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="First name" variant="filled" error={errorFirstName} helperText={errorFirstName?'This field cannot be empty':'  '}/>
           </div>
           <div className="mb-3">
-          <TextField value={lastName} onChange={(e)=>{setLastName(e.target.value)}} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Last name" variant="filled" />
+          <TextField value={lastName} onChange={(e)=>{setLastName(e.target.value); lastName!=""?setErrorLastName(false):setErrorLastName(true)}} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Last name" variant="filled" error={errorLastName} helperText={errorLastName?"This field cannot be empty":""} />
           </div>
           <div className="mb-3">
-          <TextField value={wtNumber} onChange={(e)=>{setWtNumber(e.target.value); wtNumber!=""?setError(false):setError(true);}} error={error} helperText={error ? 'This field cannot be empty' : ''} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Whatsapp Number" variant="filled" />
+          <TextField value={wtNumber} onChange={(e)=>{setWtNumber(e.target.value); wtNumber!=""? setErrorWhNumber(false): setErrorWhNumber(true);}} error={errorWhNumber} helperText={errorWhNumber ? 'This field cannot be empty' : ''} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Whatsapp Number" variant="filled" />
           </div>
           <div className="mb-3">
-          <TextField value={mobileNo} onChange={(e)=>{setMobileNo(e.target.value)}} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Mobile no" variant="filled" />
+          <TextField value={mobileNo} error={errorMobileNo} helperText={errorMobileNo?"This field cannot be empty":""} onChange={(e)=>{setMobileNo(e.target.value); mobileNo!=""?setErrorMobileNo(false):setErrorMobileNo(true)}} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Mobile no" variant="filled" />
           </div>
           <div className="mb-3">
-          <TextField value={email} onChange={(e)=>{setEmail(e.target.value)}} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Email" variant="filled" />
+          <TextField value={email} error={errorEmail} helperText={errorEmail?"This field cannot be empty":""} onChange={(e)=>{setEmail(e.target.value); email!=""?setErrorEmail(false):setErrorEmail(true)}} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Email" variant="filled" />
           </div>
           <div className="mb-3">
-          <TextField value={password} onChange={(e)=>{setPassword(e.target.value)}} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Password" variant="filled" />
+          <TextField value={password} error={errorPassword} helperText={errorPassword?"This field cannot be empty":""} onChange={(e)=>{setPassword(e.target.value); password!=""?setErrorPassword(false):setErrorPassword(true)}} sx={{bgcolor:"white"}} className="w-[500px]" id="filled-basic" label="Password" variant="filled" />
           </div>
           <div>
             <button onClick={handleSubmit} className="bg-blue-400 w-[500px] mt-5 h-9 font-bold hover:bg-blue-800">Submit</button>
