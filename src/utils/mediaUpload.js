746cfec30efefd48f1e50b12a8f14903f6e6f1f5
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-10-28 22:36:17 +0530

    add firebase file uplaod

---
+++ src/utils/mediaUpload.js
@@ -0,0 +1,24 @@
+import app from '../config/firebase'
+import { getDownloadURL, getStorage,ref, uploadBytes } from 'firebase/storage';
+
+const firebaseApp=app;
+const storage = getStorage(firebaseApp, "gs://hotel-management-89-bae73.appspot.com");
+
+
+export default async function uploadMedia(file){
+    if(file==null){
+        return
+    }
+    const fileRef=ref(storage,file.name);
+    try{
+        const snapshot=await uploadBytes(fileRef,file);
+        const url =await getDownloadURL(snapshot.ref);
+        console.log(url);
+        return url;
+    }
+    catch(e){
+        console.error("error file uploding",e)
+    }
+    
+}
+
