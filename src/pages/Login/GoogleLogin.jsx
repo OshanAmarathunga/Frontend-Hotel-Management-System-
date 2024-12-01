6094588795bfeac9bebc97c48ebe20c93e644739
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-12-01 18:37:56 +0530

    fix

--- src/pages/Login/GoogleLogin.jsx
+++ src/pages/Login/GoogleLogin.jsx
@@ -4,29 +4,35 @@ import { auth } from "../../config/firebase";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
 
 export default function GoogleLogin() {
   const navigate = useNavigate();
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
   function googleLogin() {
-    
-    
     const provider = new GoogleAuthProvider();
-    
-    
+
     signInWithPopup(auth, provider)
       .then(async (result) => {
-        const token =result.user.accessToken;
+        const token = result.user.accessToken;
         console.log("token");
-        
 
         axios
           .post(
             import.meta.env.VITE_BACKEND_URL + "/api/users/googleLogin/" + token
           )
           .then((rsp) => {
-            console.log("rsp");
-            
+            handleClose();
+
             Swal.fire({
               title: "Login!",
               text: "Login Successful !",
@@ -41,8 +47,8 @@ export default function GoogleLogin() {
             }
           })
           .catch((e) => {
-            console.log("error");
-            
+            handleClose();
+
             Swal.fire(
               "User not Found !, If you have not registered yet, please register!"
             );
@@ -57,8 +63,17 @@ export default function GoogleLogin() {
 
   return (
     <div>
+      <Backdrop
+        sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+        open={open}
+        onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <button
-        onClick={googleLogin}
+        onClick={()=>{googleLogin
+          handleOpen}
+        }
         className="absolute left-[14%] w-[70%] flex items-center justify-center hover:bg-gray-400 bg-white text-white h-[40px] bottom-[35px] rounded-md shadow-md transition duration-300 ease-in-out"
       >
         <img src="icon.png" alt="Google Icon" className="w-5 h-5 mr-2" />
