f58bb0019896f3abd8a7f3eeb8227ff5a93f9ced
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2025-01-10 13:37:22 +0530

    bug fixed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -60,7 +60,7 @@ export default function Login() {
       <Backdrop
         sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
         open={open}
-        onClick={handleClose}
+        
       >
         <CircularProgress color="inherit" />
       </Backdrop>
